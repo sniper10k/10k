e5bc88f81d57a659aeffe8a2bb759162c3accf72
Ackerman <108977326+sniper10k@users.noreply.github.com>
2022-08-30 09:29:33 +0800

    Add files via upload

--- 三步翻转法.c
+++ 三步翻转法.c
@@ -20,15 +20,15 @@ void leftmove(char* arr, int k)
 	assert(arr);
 	int len = strlen(arr);
 	assert(k <= len);
-	reverse(arr, arr + k - 1);
-	reverse(arr + k, arr + len - 1);
-	reverse(arr, arr + len - 1);
+	reverse(arr, arr + k - 1);		//前k个字符的逆序
+	reverse(arr + k, arr + len - 1);//k后面字符的逆序
+	reverse(arr, arr + len - 1);	//整体字符的逆序
 }
 int main()
 {
 	char arr[] = "abcdef";
 	leftmove(arr, 2);
-	printf("%s", arr);
+	printf("%s\n", arr);
 
 	return 0;
 }
