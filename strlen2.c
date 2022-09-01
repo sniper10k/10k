1318a0645b68db031bfac0c978f6bcc9379ad279
Ackerman <108977326+sniper10k@users.noreply.github.com>
2022-09-01 21:23:04 +0800

    字符串长度递归写法

---
+++ strlen2.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int my_strlen(const char* str)    //不能定义变量
+{
+    if (*str == '\0')
+        return 0;
+    else
+        return 1 + my_strlen(str + 1);
+}
+int main()
+{
+    char arr[] = "abcdef";
+    int len = my_strlen(arr);
+    printf("%d\n", len);
+    return 0;
+}
