053e3ccccfc04bbd1cfdf92a5000e7145337afd6
Ackerman <108977326+sniper10k@users.noreply.github.com>
2022-09-01 21:21:00 +0800

    字符串长度计数器写法

--- strlen1.c
+++ strlen1.c
@@ -1,23 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<assert.h>
-char* my_strcpy(char* arr1, const char* arr2)
+int my_strlen(const char* str)
 {
-    assert(arr1 != NULL);
-    assert(arr2 != NULL);
-    char* ret = arr1;    //选取arr1的地址,最后返回地址,所以用char*接收
-    while (*arr1++ = *arr2++);
-    //while(*arr2=='\0')
-    //    *arr1=*arr2;
-    //    arr1++;
-    //    arr2++;        
-    return ret;
+    int count = 0;
+    assert(str != NULL);
+    while (*str)    //*str!='\0'
+    {
+        count++;
+        str++;    //地址++,不能解引用    
+    }
+    return count;
 }
 int main()
 {
-    char arr1[] = "abcdefghi";
-    char arr2[] = "bit";
-    my_strcpy(arr1, arr2);
-    printf("%s", arr1);
+    char arr[] = "abcdef";
+    int len = my_strlen(arr);
+    printf("%d\n", len);
     return 0;
 }
