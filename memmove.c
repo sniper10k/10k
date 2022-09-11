6ba6a99252da8c6a72f91938069dbaca11c3a3cd
Ackerman <108977326+sniper10k@users.noreply.github.com>
2022-09-11 10:46:02 +0800

    my_memmove实现

---
+++ memmove.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+void* my_memmove(void* dest, const void* src, size_t count)
+{
+	void* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	if (dest < src)
+	{
+		while (count--)
+		{
+			*(char*)dest = *(char*)src;
+			++(char*)dest;
+			++(char*)src;
+		}
+	}
+	else
+	{
+		while (count--)
+		{
+			*((char*)dest + count) = *((char*)src + count);
+
+		}
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+	my_memmove(arr1 + 2, arr1, 20);
+	return 0;
+}
